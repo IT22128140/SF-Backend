4fe5d37ddc45e062cfe076ade0b5abf321a774e8
hiranya21 <128223556+hiranya21@users.noreply.github.com>
2024-04-04 14:38:17 +0530

    push#4BE

--- index.js
+++ index.js
@@ -1,25 +1,195 @@
-import express, { request, response } from "express";
+import express from "express";
 import { PORT, MONGO_URI } from "./config.js";
-import mongoose, { pluralize } from "mongoose";
-import { Repair } from "./models/repairModel.js";
+import mongoose from "mongoose";
+import cors from "cors";
+//enter your imports under your name. don't add or delete empty spaces
+//Sageevan
+
+
+
+
+
+
+
+
+
+
+//Varagan
+
+
+
+
+
+
+
+
+
+
+//Hiranya
 import repairsRoute from './routes/repairsRoute.js';
-import { Machine } from "./models/machinesModes.js";
 import machinesRoute from './routes/machinesRoute.js';
 import mprShortagesRoute from './routes/mprShortagesRoute.js';
 
+
+
+
+
+
+
+//Ridmi
+
+
+
+
+
+
+
+
+
+
+//Isuru
+
+
+
+
+
+
+
+
+
+
+//Gihan
+
+
+
+
+
+
+
+
+
+//
+
+
+
+
+
+
+
+
+
+//Maneth
+
+
+
+
+
+
+
+
+
+
+
+//connection
 const app = express();
-
-//Middleware for parsing request body
 app.use(express.json());
-
+app.use(cors());
 app.get("/", (req, res) => {
   console.log(req);
-  return res.status(234).send("Welcome To MERN Stack Tutorial");
+  return res.status(234).send("Connection Successful!");
 });
 
+//enter your routes under your name. don't add or delete empty spaces
+//Varagan
+
+
+
+
+
+
+
+
+
+
+//Sageevan
+
+
+
+
+
+
+
+
+
+
+//Hiranya
 app.use('/repairs', repairsRoute);
 app.use('/machines', machinesRoute);
 app.use('/mpshortages', mprShortagesRoute);
+
+
+
+
+
+
+
+
+//Ridmi
+
+
+
+
+
+
+
+
+
+
+//Isuru
+
+
+
+
+
+
+
+
+
+
+//Gihan
+
+
+
+
+
+
+
+
+
+//Sandithi
+
+
+
+
+
+
+
+
+
+//Maneth
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 mongoose
   .connect(MONGO_URI)
@@ -30,5 +200,5 @@ mongoose
     });
   })
   .catch((error) => {
-    console.log("MongoDB Connection Error: ", error);
+    console.log("MongoDB Connection Error: ", error);                                    // Logging MongoDB connection error
   });
