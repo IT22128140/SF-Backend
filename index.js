6f6fca146bfd41d3876d83f269a2d28bf4edcdba
IT22128140 <it22128140@my.sliit.lk>
2024-04-04 15:55:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -26,9 +26,9 @@ import cors from "cors";
 
 
 //Hiranya
-
-
-
+import repairsRoute from './routes/repairsRoute.js';
+import machinesRoute from './routes/machinesRoute.js';
+import mprShortagesRoute from './routes/mprShortagesRoute.js';
 
 
 
@@ -84,35 +84,6 @@ import cartRoute from "./routes/cartRoute.js";
 import deliveryRoute from "./routes/deliveryDetailsRoute.js";
 import mongoose from "mongoose";
 import cors from "cors";
-//enter your imports under your name. don't add or delete empty spaces
-//Sageevan
-
-
-
-
-
-
-
-
-
-
-//Varagan
-
-
-
-
-
-
-
-
-
-
-//Hiranya
-import repairsRoute from './routes/repairsRoute.js';
-import machinesRoute from './routes/machinesRoute.js';
-import mprShortagesRoute from './routes/mprShortagesRoute.js';
-
-
 
 
 
@@ -153,9 +124,9 @@ app.get("/", (req, res) => {
 
 
 //Hiranya
-
-
-
+app.use('/repairs', repairsRoute);
+app.use('/machines', machinesRoute);
+app.use('/mpshortages', mprShortagesRoute);
 
 
 
@@ -208,97 +179,6 @@ app.get("/", (req, res) => {
 app.use("/items", itemsRoute);
 app.use("/cart", cartRoute);
 app.use("/deliveryDetails", deliveryRoute);
-
-
-
-
-
-
-
-
-
-
-
-//enter your routes under your name. don't add or delete empty spaces
-//Varagan
-
-
-
-
-
-
-
-
-
-
-//Sageevan
-
-
-
-
-
-
-
-
-
-
-//Hiranya
-app.use('/repairs', repairsRoute);
-app.use('/machines', machinesRoute);
-app.use('/mpshortages', mprShortagesRoute);
-
-
-
-
-
-
-
-
-//Ridmi
-
-
-
-
-
-
-
-
-
-
-//Isuru
-
-
-
-
-
-
-
-
-
-
-//Gihan
-
-
-
-
-
-
-
-
-
-//Sandithi
-
-
-
-
-
-
-
-
-
-//Maneth
-
-
 
 
 
