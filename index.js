4e328a1846c850d0ad753ff5ad861014af53691a
IT22106506 <it22106506@my.sliit.lk>
2024-04-04 14:38:13 +0530

    index s

--- index.js
+++ index.js
@@ -1,32 +1,192 @@
 import express from "express";
 import { PORT, MONGO_URI } from "./config.js";
 import mongoose from "mongoose";
+import cors from "cors";
+//enter your imports under your name. don't add or delete empty spaces
+//Sageevan
+
+
+
+
+
+
+
+
+
+
+//Varagan
+
+
+
+
+
+
+
+
+
+
+//Hiranya
+
+
+
+
+
+
+
+
+
+//Ridmi
+
+
+
+
+
+
+
+
+
+
+//Isuru
+
+
+
+
+
+
+
+
+
+
+//Gihan
+
+
+
+
+
+
+
+
+
+//Sandithi
 import employeeRoute from "./routes/employeeRoute.js";
 import attendanceRoute from "./routes/attendanceRoute.js";
 import occupationRoute from "./routes/occupationRoute.js";
-import cors from "cors";
 
+
+
+
+
+
+//Maneth
+
+
+
+
+
+
+
+
+
+
+
+//connection
 const app = express();
-
 app.use(express.json());
-
 app.use(cors());
-// app.use(
-//   cors({
-//     origin: "http://localhost:5555",
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//     allowedHeaders: ["Content-Type"],
-//   })
-// );
-
 app.get("/", (req, res) => {
   console.log(req);
   return res.status(234).send("Connection Successful!");
 });
 
+//enter your routes under your name. don't add or delete empty spaces
+//Varagan
+
+
+
+
+
+
+
+
+
+
+//Sageevan
+
+
+
+
+
+
+
+
+
+
+//Hiranya
+
+
+
+
+
+
+
+
+
+//Ridmi
+
+
+
+
+
+
+
+
+
+
+//Isuru
+
+
+
+
+
+
+
+
+
+
+//Gihan
+
+
+
+
+
+
+
+
+
+//Sandithi
 app.use("/employee", employeeRoute);
 app.use("/attendance", attendanceRoute);
 app.use("/occupation", occupationRoute);
+
+
+
+
+
+
+//Maneth
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 mongoose
   .connect(MONGO_URI)
@@ -37,5 +197,5 @@ mongoose
     });
   })
   .catch((error) => {
-    console.log("MongoDB Connection Error: ", error);
+    console.log("MongoDB Connection Error: ", error);                                    // Logging MongoDB connection error
   });
