3382b7971fe945b8b9f313814f83f98931f145e6
IT22106506 <it22106506@my.sliit.lk>
2024-04-04 10:32:46 +0530

    employee model and routes

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 import express from "express";
 import { PORT, MONGO_URI } from "./config.js";
 import mongoose from "mongoose";
+import employeeRoute from "./routes/employeeRoute.js";
+import attendanceRoute from "./routes/attendanceRoute.js";
+import occupationRoute from "./routes/occupationRoute.js";
+import cors from "cors";
 
 const app = express();
 
+app.use(express.json());
+
+app.use(cors());
+// app.use(
+//   cors({
+//     origin: "http://localhost:5555",
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     allowedHeaders: ["Content-Type"],
+//   })
+// );
+
 app.get("/", (req, res) => {
   console.log(req);
-  return res.status(234).send("Welcome To MERN Stack Tutorial");
+  return res.status(234).send("Connection Successful!");
 });
+
+app.use("/employee", employeeRoute);
+app.use("/attendance", attendanceRoute);
+app.use("/occupation", occupationRoute);
 
 mongoose
   .connect(MONGO_URI)
