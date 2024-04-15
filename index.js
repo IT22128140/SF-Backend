40c1a0439578bb7a5cfbe4d3c6beae49598e12bc
IT22289216 <it22289216@my.sliit.lk>
2024-04-15 11:20:45 +0530

    h

--- index.js
+++ index.js
@@ -1,13 +1,196 @@
-import express from "express";
+import express, { request, response } from "express";
 import { PORT, MONGO_URI } from "./config.js";
 import mongoose from "mongoose";
+import cors from "cors";
+//enter your imports under your name. don't add or delete empty spaces
+//Sageevan
 
+
+
+
+
+
+
+
+
+
+//Varagan
+
+
+
+
+
+
+
+
+
+
+//Hiranya
+
+
+
+
+
+
+
+
+
+//Ridmi
+
+
+
+
+
+
+
+
+
+
+//Isuru
+
+
+
+
+
+
+
+
+
+
+
+//Gihan
+
+
+
+
+
+
+
+
+
+//Sandithi
+
+
+
+
+
+
+
+
+
+//Maneth
+import itemsRoute from "./routes/itemsRoute.js";
+import cartRoute from "./routes/cartRoute.js";
+import deliveryRoute from "./routes/deliveryDetailsRoute.js";
+
+
+
+
+
+
+
+
+
+//connection
 const app = express();
-
+app.use(express.json());
+app.use(cors());
 app.get("/", (req, res) => {
   console.log(req);
-  return res.status(234).send("Welcome To MERN Stack Tutorial");
+  return res.status(234).send("Connection Successful!");
 });
+
+//enter your routes under your name. don't add or delete empty spaces
+//Varagan
+
+
+
+
+
+
+
+
+
+
+//Sageevan
+
+
+
+
+
+
+
+
+
+
+//Hiranya
+
+
+
+
+
+
+
+
+
+//Ridmi
+
+
+
+
+
+
+
+
+
+
+//Isuru
+
+
+
+
+
+
+
+
+
+
+//Gihan
+
+
+
+
+
+
+
+
+
+//Sandithi
+
+
+
+
+
+
+
+
+
+//Maneth
+app.use("/items", itemsRoute);
+app.use("/cart", cartRoute);
+app.use("/deliveryDetails", deliveryRoute);
+
+
+
+
+
+
+//varagan
+app.use("/Feedback", FeedbackFormRoute);
+
+
+
+
 
 mongoose
   .connect(MONGO_URI)
@@ -18,110 +201,5 @@ mongoose
     });
   })
   .catch((error) => {
-    console.log("MongoDB Connection Error: ", error);
-  });
-
-
-
-  app.post('/api/RegisterEmp', async (req,res) => {
-    mongoose.connect(process.env.MONGO_URL);
-    const {FirstName,LaststName,emailAddress,phoneNumber,password,password2,EmployeeType} = req.body;
-  
-    try {
-      const userDoc = await User.create({
-        FirstName,
-        LaststName,
-        emailAddress,
-        phoneNumber,
-        password,
-        EmployeeType,
-        password2:bcrypt.hashSync(password,password2, bcryptSalt),
-      });
-      res.json(userDoc);
-    } catch (e) {
-      res.status(422).json(e);
-    }
-  
-  });
-
-  app.post('/api/RegisterCus', async (req,res) => {
-    mongoose.connect(process.env.MONGO_URL);
-    const {FirstName,LaststName,emailAddress,phoneNumber,password,password2} = req.body;
-  
-    try {
-      const userDoc = await User.create({
-        FirstName,
-        LaststName,
-        emailAddress,
-        phoneNumber,
-        password,
-        password2:bcrypt.hashSync(password,password2, bcryptSalt),
-      });
-      res.json(userDoc);
-    } catch (e) {
-      res.status(422).json(e);
-    }
-  
-  });
-  
-  app.post('/api/LoginCus', async (req,res) => {
-    mongoose.connect(process.env.MONGO_URL);
-    const {email,password} = req.body;
-    const userDoc = await User.findOne({email});
-    if (userDoc) {
-      const passOk = bcrypt.compareSync(password, userDoc.password);
-      if (passOk) {
-        jwt.sign({
-          email:userDoc.email,
-          id:userDoc._id
-        }, jwtSecret, {}, (err,token) => {
-          if (err) throw err;
-          res.cookie('token', token).json(userDoc);
-        });
-      } else {
-        res.status(422).json('pass not ok');
-      }
-    } else {
-      res.json('not found');
-    }
-  });
-
-  app.post('/api/LoginEmp', async (req,res) => {
-    mongoose.connect(process.env.MONGO_URL);
-    const {email,password} = req.body;
-    const userDoc = await User.findOne({email});
-    if (userDoc) {
-      const passOk = bcrypt.compareSync(password, userDoc.password);
-      if (passOk) {
-        jwt.sign({
-          email:userDoc.email,
-          id:userDoc._id
-        }, jwtSecret, {}, (err,token) => {
-          if (err) throw err;
-          res.cookie('token', token).json(userDoc);
-        });
-      } else {
-        res.status(422).json('pass not ok');
-      }
-    } else {
-      res.json('not found');
-    }
-  });
-  
-  app.get('/api/profile', (req,res) => {
-    mongoose.connect(process.env.MONGO_URL);
-    const {token} = req.cookies;
-    if (token) {
-      jwt.verify(token, jwtSecret, {}, async (err, userData) => {
-        if (err) throw err;
-        const {FirstName,LaststName,emailAddress,phoneNumber} = await User.findById(userData.id);
-        res.json({FirstName,LaststName,emailAddress,phoneNumber});
-      });
-    } else {
-      res.json(null);
-    }
-  });
-  
-  app.post('/api/logout', (req,res) => {
-    res.cookie('token', '').json(true);
+    console.log("MongoDB Connection Error: ", error);                                    // Logging MongoDB connection error
   });
