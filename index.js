d09017e8f6695c5300d8113dbd869a940aaab09e
IT22289216 <it22289216@my.sliit.lk>
2024-04-21 11:53:53 +0530

    hi

--- index.js
+++ index.js
@@ -4,10 +4,7 @@ import { PORT, MONGO_URI } from "./config.js";      // Importing PORT and MONGO_
 import cors from "cors";
 //enter your imports under your name. don't add or delete empty spaces
 //Sageevan
-import productRequestRoute from "./routes/productRequestRoute.js";//sageevanRoute 1/4
-import productReviewRoute from "./routes/productReviewRoute.js";//sageevanRoute 2/4
-import rejectedProductRoute from "./routes/rejectedProductRoute.js";//sageevanRoute 3/4
-import releaseProductRoute from "./routes/releaseProductRoute.js";//sageevanRoute 4/4
+
 
 
 
@@ -27,10 +24,7 @@ import FeedbackFormRoute from "./routes/FeedbackFormRoute.js";
 
 
 //Hiranya
-import repairsRoute from './routes/repairsRoute.js';
-import machinesRoute from './routes/machinesRoute.js';
-import mprShortagesRoute from './routes/mprShortagesRoute.js';
-import MaintenanceRoute from './routes/MaintenanceRoute.js';
+
 
 
 
@@ -38,10 +32,6 @@ import MaintenanceRoute from './routes/MaintenanceRoute.js';
 
 
 //Ridmi
-import rmRequestRoute from './routes/rmRequestRoute.js';
-import rmDistributeRoute from './routes/rmDistributeRoute.js';
-import empPerformanceRoute from './routes/empPerformanceRoute.js';
-
 
 
 
@@ -49,11 +39,6 @@ import empPerformanceRoute from './routes/empPerformanceRoute.js';
 
 
 //Isuru
-import SupplierDetailsroutes from "./routes/SupplierDetailsroutes.js";
-import RMStockRoutes from "./routes/RMStockRoutes.js";
-import MachinePartRoutes from "./routes/MachinePartRoutes.js";
-import RequestFillingRoutes from "./routes/RequestFillingRoutes.js"
-import suppRawsRoute from "./routes/suppRawsRoute.js"
 
 
 
@@ -74,32 +59,16 @@ import suppRawsRoute from "./routes/suppRawsRoute.js"
 
 
 //Sandithi
-import employeeRoute from "./routes/employeeRoute.js";
-import attendanceRoute from "./routes/attendanceRoute.js";
-import occupationRoute from "./routes/occupationRoute.js";
-import resignRoute from "./routes/resignRoute.js";
-import employeeStatusRoute from "./routes/employeeStatusRoute.js";
 
 
 
 
 //Maneth
-import itemsRoute from "./routes/itemsRoute.js";
-import cartRoute from "./routes/cartRoute.js";
-import deliveryRoute from "./routes/deliveryDetailsRoute.js";
-import orderRoute from "./routes/orderRoute.js";
 
 
 
 
 
-
-<<<<<<< HEAD
-
-
-
-=======
->>>>>>> cc983efe75397a4e959af4433d780406a2a2818a
 
 
 //connection
@@ -124,21 +93,13 @@ app.use("/Feedback", FeedbackFormRoute);
 
 
 //Sageevan
-app.use('/qualityControl/productRequest',productRequestRoute);
-app.use('/qualityControl/productReview',productReviewRoute);
-app.use('/qualityControl/releaseProduct',releaseProductRoute);
-app.use('/qualityControl/rejectedProduct',rejectedProductRoute);
-
 
 
 
 
 
 //Hiranya
-app.use('/repairs', repairsRoute);
-app.use('/machines', machinesRoute);
-app.use('/mpshortages', mprShortagesRoute);
-app.use('/maintenance', MaintenanceRoute);
+
 
 
 
@@ -146,9 +107,7 @@ app.use('/maintenance', MaintenanceRoute);
 
 
 //Ridmi
-app.use('/rmRequests', rmRequestRoute);
-app.use('/rmDistributes', rmDistributeRoute);
-app.use('/empPerformances', empPerformanceRoute);
+
 
 
 
@@ -157,11 +116,7 @@ app.use('/empPerformances', empPerformanceRoute);
 
 
 //Isuru
-app.use('/supdetails', SupplierDetailsroutes);
-app.use('/RMstock', RMStockRoutes);
-app.use('/mpstock', MachinePartRoutes);
-app.use('/ReqFF', RequestFillingRoutes);
-app.use('/suppRM',suppRawsRoute);
+
 
 
 
@@ -183,20 +138,13 @@ app.use('/suppRM',suppRawsRoute);
 
 
 //Sandithi
-app.use("/employee", employeeRoute);
-app.use("/attendance", attendanceRoute);
-app.use("/occupation", occupationRoute);
-app.use("/resign", resignRoute);
-app.use("/employeeStatus", employeeStatusRoute);
+
 
 
 
 
 //Maneth
-app.use("/items", itemsRoute);
-app.use("/cart", cartRoute);
-app.use("/deliveryDetails", deliveryRoute);
-app.use("/order", orderRoute);
+
 
 
 
